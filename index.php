0813f5e57711d2455258a4c045ed0d95b3dabc92
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-16 22:17:05 +0300

    .

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login | I&D CREW</title>
+  <link rel="icon" href="/logos/logo512.jpg">
   <link rel="stylesheet" href="style.css">
   <!-- Bootstrap -->
   <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
--- profile.php
+++ profile.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PROFIL</title>
+    <link rel="icon" href="/logos/logo512.jpg">
     <link rel="stylesheet" href="css/profile.css">
     <link rel="manifest" href="/manifest.json">
 </head>
