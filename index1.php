064a7797a239fdb74055a8fd33272ca3b0a5635d
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-16 22:26:18 +0300

    ..

--- index1.php
+++ index1.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Register | I&D CREW</title>
-  <link rel="icon" href="/logos/logo512.jpg">
+  <link rel="icon" href="/logos/logo192.jpg">
   <link rel="stylesheet" href="style.css">
   <!-- Bootstrap -->
   <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
