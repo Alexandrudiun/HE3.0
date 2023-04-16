22466783a07b76a95b4749dff86375144a83d40d
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-16 13:44:33 +0300

    .

--- profile.php
+++ profile.php
@@ -5,11 +5,51 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PROFIL</title>
+    <link rel="stylesheet" href="css/profile.css">
     <link rel="manifest" href="/manifest.json">
 </head>
 <body>
-    <h1>Pagina din aplicatie</h1>
-<div id="message"></div>
+    <div id="message"></div>
+    <section>
+        <div class="navigation">
+            <ul>
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                        <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="text">Acasă</span>
+                    </a>
+                </li>
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                        <ion-icon name="person-circle-outline"></ion-icon>
+                        </span>
+                        <span class="text">Profil</span>
+                    </a>
+                </li>
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                        <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
+                        </span>
+                        <span class="text">Chat</span>
+                    </a>
+                </li>
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                        <ion-icon name="settings-outline"></ion-icon>
+                        </span>
+                        <span class="text">Setări</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
 <?php
