b48b9fbbddf8e5f79c345fc2c591835988d7215e
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-18 16:23:59 +0300

    .

---
+++ profil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PROFIL</title>
+    <link rel="icon" href="/logos/logo192.jpg">
+    <link rel="stylesheet" href="css/profil.css">
+    <link rel="manifest" href="/manifest.json">
+</head>
+<body>
+  <header>
+      <div class="banner-container">
+      <img src="logos/user.png" alt="user-image" class="user">
+      <a href="#">Editeaza</a>
+    </div>
+  </header>
+
+  <main>
+
+  </main>
+</body>
+</html>
