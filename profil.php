b23f7150939877678939a8bbcd8bbbf178b107df
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-18 17:47:48 +0300

    Update profil.php

--- profil.php
+++ profil.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PROFIL</title>
+    <title>Profilul meu</title>
     <link rel="icon" href="/logos/logo192.jpg">
     <link rel="stylesheet" href="css/profil.css">
     <link rel="manifest" href="/manifest.json">
