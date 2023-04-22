8ca2c40d9605806d929095c608c87012912b41a3
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-04-22 23:25:48 +0300

    1

--- frontend/profil.php
+++ frontend/profil.php
@@ -14,34 +14,7 @@
     <main>
         <div class="center">
     <div class="profile-card">
-        <div class="image" id="image"><?php
-         include "conn.php";
-         session_start(); // Start the session
-         if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
-             $email = $_SESSION['email'];
-             $password = $_SESSION['password'];
-         
-             $query="SELECT * FROM users WHERE email = '{$email}'";
-             $select_user_query = mysqli_query($conn, $query);
-             
-             if(!$select_user_query)
-             {
-               die('Query Failed'. mysqli_error());
-             }
-             $email_found = false; // Flag variable to check if email was found
-             while($row = mysqli_fetch_row($select_user_query)) {
-             if($row[1] == $email && $row[2] == $password) {
-               $email_found = true;
-               $image = $row[3];
-               $name = $row[4];
-               $skills = $row[5];
-               echo "<img src='data:image/jpeg;base64," . base64_encode($image) . "' />";
-               echo "<script>document.getElementById('name').innerHTML = '{$name} ';</script>";
-               echo "<script>document.getElementById('skills').innerHTML = '{$skills}';</script>";
-             
-             }
-         } ?>
-         </div>
+        <div class="image" id="image"></div>
         <div class="text-data">
             <!-- Aici vine php cu echo si script pentru email -->
             <span class="name"><div id="name"></div></span>
@@ -151,3 +124,4 @@
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
+<?php include "../backend/profilbk.php" ?>
