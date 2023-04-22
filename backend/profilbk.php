4909564ec719f79f7814f8e6e7ec0bc2f2ccb7c9
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-04-22 22:15:43 +0300

    1

--- backend/profilbk.php
+++ backend/profilbk.php
@@ -1,12 +1,29 @@
 <?php
- 
+ include "conn.php";
 session_start(); // Start the session
 if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
     $email = $_SESSION['email'];
     $password = $_SESSION['password'];
-   // echo "Datele contului logat:<br>";
-    echo "<script>document.getElementById('message').innerHTML = '{$email}';</script>";
-    //echo "Password: " . $password;
+
+    $query="SELECT * FROM users WHERE email = '{$email}'";
+    $select_user_query = mysqli_query($conn, $query);
+    
+    if(!$select_user_query)
+    {
+      die('Query Failed'. mysqli_error());
+    }
+    $email_found = false; // Flag variable to check if email was found
+    while($row = mysqli_fetch_row($select_user_query)) {}
+    if($row[1] == $email && $row[2] == $password) {
+      $email_found = true;
+      $image = $row[3];
+      $name = $row[4];
+      $skills = $row[5];
+      echo "<script>document.getElementById('image').innerHTML = '{$image}';</script>";
+      echo "<script>document.getElementById('name').innerHTML = '{$name}';</script>";
+      echo "<script>document.getElementById('skills').innerHTML = '{$skills}';</script>";
+    
+    }
 }
 
 
--- frontend/profil.php
+++ frontend/profil.php
@@ -10,7 +10,80 @@
     <link rel="manifest" href="/manifest.json">
 </head>
 <body>
-  <header>
+    <main>
+        <div class="center">
+    <div class="profile-card">
+        <div class="image" id="image">
+            <img src="/img/user.png" alt="user image" class="profile-img">
+        </div>
+        <div class="text-data">
+            <!-- Aici vine php cu echo si script pentru email -->
+            <span class="name"><div id="name"></div></span>
+            <span class="jobs"><div id="skills"></div></span>
+        </div>
+    </div>
+        </div>
+
+    <div class="btn-container">
+        <a href="post.php" class="btn-post">Publică un anunţ</a>
+    </div>
+    
+    </main>
+    <footer>
+        <!-- Navbar Down -->
+    <section class="nav-bar">
+        <div class="navigation">
+            <ul>
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                        <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="text">Acasă</span>
+                    </a>
+                </li>
+                <li class="list active">
+                    <a href="#">
+                        <span class="icon">
+                        <ion-icon name="person-circle-outline"></ion-icon>
+                        </span>
+                        <span class="text">Profil</span>
+                    </a>
+                </li>
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="add-circle-outline"></ion-icon>
+                        </span>
+                        <span class="text">Adaugă anunţ</span>
+                    </a>
+                </li>
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="albums-outline"></ion-icon>
+                        </span>
+                        <span class="text">Istoric</span>
+                    </a>
+                </li>
+                <li class="list">
+                    <a href="#">
+                        <span class="icon">
+                        <ion-icon name="build-outline"></ion-icon>
+                        </span>
+                        <span class="text">My work</span>
+                    </a>
+                </li>
+                <div class="indicator"></div>
+            </ul>
+        </div>
+    </section>
+    </footer>
+
+
+
+    <!-- Old design -->
+  <!-- <header>
       <div class="banner-container">
           <img src="/img/user.png" alt="user-image" class="user">
           <a href="#" class="edit">EDITEAZĂ</a>
@@ -23,7 +96,6 @@
   <div class="btn-container">
   <a href="post.php" class="btn-post">Publică un anunţ</a>
   </div>
-  <!-- Anunturi -->
   <main>
     <section class="section-1">
       <h3 class="title">Anunţurile tale</h3>
@@ -46,7 +118,8 @@
         </a>
       </div>
     </section>
-  </main>
+  </main> -->
+  <script src="/js/nav.js"></script>
   <!-- Ion icons -->
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
