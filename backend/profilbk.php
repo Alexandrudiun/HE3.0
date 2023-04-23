304bd0e87b5a5a3438598e527e3218dbf9a00601
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-04-23 10:49:36 +0300

    Update profilbk.php

--- backend/profilbk.php
+++ backend/profilbk.php
@@ -1,7 +1,5 @@
 <?php  
 include "conn.php";
-if($conn){echo "OK";}
-  else{die("not ok");}
 
 session_start(); // Start the session
 if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
@@ -22,9 +20,10 @@ if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
       $image = $row[3];
       $name = $row[4];
       $skills = $row[5];
+      echo "<script>document.getElementById('image').innerHTML = '<img src=\'data:image/jpeg;base64," . base64_encode($image) . "\' alt=\'user image\' class=\'profile-img\'>';</script>";
       echo "<img src='data:image/jpeg;base64," . base64_encode($image) . "' />";
       echo "<script>document.getElementById('name').innerHTML = '{$name}';</script>";
       echo "<script>document.getElementById('skills').innerHTML = '{$skills}';</script>";
     }
     }
-}
+}?>
