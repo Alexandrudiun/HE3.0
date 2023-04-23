0ec598908926df2573187f094facf72e2bca01e9
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-04-23 23:40:46 +0300

    Update logoutbk.php

--- backend/logoutbk.php
+++ backend/logoutbk.php
@@ -1,10 +1,10 @@
 <!-- https://www.php.net/session_destroy -->
 <?php 
 function logoutanddeletecookies() {
-  session_start();
-  session_destroy();
-  setcookie("email", "", time() - 3600);
-  setcookie("password", "", time() - 3600);
+//   session_start();
+//   session_destroy();
+//   setcookie("email", "", time() - 3600);
+//   setcookie("password", "", time() - 3600);
   header("Location: ../index.php");
 }
 logoutanddeletecookies();
