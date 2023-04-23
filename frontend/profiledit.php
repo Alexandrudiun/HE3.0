f9f9d841ebc82cb7efcd4473395817bda41daafc
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-23 14:51:23 +0300

    Update profiledit.php

--- frontend/profiledit.php
+++ frontend/profiledit.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profilul meu</title>
     <link rel="icon" href="/logos/logo192.jpg">
-    <link rel="stylesheet" href="/css/profiledit">
+    <link rel="stylesheet" href="/css/profiledit.css">
     <link rel="stylesheet" href="/css/navbar.css">
     <link rel="manifest" href="/manifest.json">
 </head>
 <body>
     <main>
-    
+
     </main>
   
     <footer>  
