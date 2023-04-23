d9d1837ff752e4f003300db7e27ef8c86f1b4d6c
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-04-23 10:42:33 +0300

    Update profil.php

--- frontend/profil.php
+++ frontend/profil.php
@@ -19,7 +19,7 @@
       </div> 
         <div class="text-data">
             <!-- Aici vine php cu echo si script pentru email -->
-            <span class="name"><div id="usr"></div></span>
+            <span class="name"><div id="name"></div></span>
             <span class="jobs"><div id="skills">1</div></span>
         </div>
     </div>
@@ -129,17 +129,32 @@
 
 
 <?php
-echo "ok1";
-session_start(); // Start the session
 
-if(isset($_SESSION['email']) && isset($_SESSION['password'])) {echo "ok1";
+include "../backend/conn.php";
+session_start(); // Start the session
+if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
     $email = $_SESSION['email'];
     $password = $_SESSION['password'];
+
+    $query="SELECT * FROM users WHERE email = '{$email}'";
+    $select_user_query = mysqli_query($conn, $query);
     
-   // echo "Datele contului logat:<br>";
-    echo "<script>document.getElementById('usr').innerHTML = '{$email}';</script>";
-    //echo "Password: " . $password;
-    
+    if(!$select_user_query)
+    {
+      die('Query Failed'. mysqli_error());
+    }
+    $email_found = false; // Flag variable to check if email was found
+    while($row = mysqli_fetch_row($select_user_query)) {
+    if($row[1] == $email && $row[2] == $password) {
+      $email_found = true;
+      $image = $row[3];
+      $name = $row[4];
+      $skills = $row[5];
+      echo "<img src='data:image/jpeg;base64," . base64_encode($image) . "' />";
+      echo "<script>document.getElementById('name').innerHTML = '{$name}';</script>";
+      echo "<script>document.getElementById('skills').innerHTML = '{$skills}';</script>";
+    }
+    }
 }
 
 
