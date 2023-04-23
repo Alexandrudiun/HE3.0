9a89e3c3dd839707093ad1635975f632ba2554ce
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-23 14:48:04 +0300

    .

--- frontend/profiledit.php
+++ frontend/profiledit.php
@@ -6,11 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profilul meu</title>
     <link rel="icon" href="/logos/logo192.jpg">
+    <link rel="stylesheet" href="/css/profiledit">
     <link rel="stylesheet" href="/css/navbar.css">
     <link rel="manifest" href="/manifest.json">
 </head>
 <body>
-  
+    <main>
+      
+    </main>
   
     <!-- Navbar Down -->
     <footer>
