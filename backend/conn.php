755962d23942ccc7c30ac9a01a3ff5808f320888
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-04-23 10:24:56 +0300

    Update conn.php

--- backend/conn.php
+++ backend/conn.php
@@ -2,6 +2,10 @@
 $dbServername="localhost";
 $dbUsername="u312656249_idcrew";
 $dbPassoword="IonescuDinuCrew2023!";
+// $dbUsername="root";
+// $dbPassoword="";
 $dbName="u312656249_idcrew";
 
 $conn= mysqli_connect($dbServername, $dbUsername, $dbPassoword, $dbName);
+
+
