019704c43200f2c4b55fe684001f308d93064662
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-04-23 10:33:15 +0300

    Update profil.php

--- frontend/profil.php
+++ frontend/profil.php
@@ -80,6 +80,46 @@
         </div>
     </section>
     </footer>
+
+
+
+    <!-- Old design -->
+  <!-- <header>
+      <div class="banner-container">
+          <img src="/img/user.png" alt="user-image" class="user">
+          <a href="#" class="edit">EDITEAZĂ</a>
+      </div>
+  </header>
+
+  <div id="message"></div>
+
+
+  <div class="btn-container">
+  <a href="post.php" class="btn-post">Publică un anunţ</a>
+  </div>
+  <main>
+    <section class="section-1">
+      <h3 class="title">Anunţurile tale</h3>
+      <a href="" class="active-link">
+      <div class="active-section">
+        <h3>Anunţuri active</h3>
+        <div class="active-section-left">
+          <a href="#">2</a>
+          <ion-icon name="arrow-forward-outline"></ion-icon>
+        </div>
+        </a>
+      </div>
+      <div class="active-section">
+        <a href="" class="active-link">
+        <h3>Anunţuri dezactivate</h3>
+        <div class="active-section-left">
+          <a href="#">5</a>
+          <ion-icon name="arrow-forward-outline"></ion-icon>
+        </div>
+        </a>
+      </div>
+    </section>
+  </main> -->
   <script src="/js/nav.js"></script>
   <!-- Ion icons -->
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
@@ -88,12 +128,32 @@
 </html>
 <?php
 
-
 session_start(); // Start the session
+include "conn.php"; 
 if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
     $email = $_SESSION['email'];
     $password = $_SESSION['password'];
-   // echo "Datele contului logat:<br>";
     echo "<script>document.getElementById('name').innerHTML = '{$email}';</script>";
-    //echo "Password: " . $password;
+    $query="SELECT * FROM users WHERE email = '{$email}'";
+    $select_user_query = mysqli_query($conn, $query);
+    
+    if(!$select_user_query)
+    {
+      die('Query Failed'. mysqli_error());
+    }
+    if(mysqli_num_rows($select_user_query) > 0) {
+        while($row = mysqli_fetch_assoc($select_user_query)) {
+            $db_email = $row['email'];
+            $db_password = $row['password'];
+            $db_name = $row['name'];
+            $db_skills = $row['skills'];
+            $db_image = $row['image'];
+        }
+    }
+    echo $db_name;
+    echo $db_skills;
+    echo $db_image;
+    echo $db_email;
+    echo $db_password;
 }
+
