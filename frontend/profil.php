5cf1dd34044f57448810c3e8681bb6ac01ebdc16
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-04-23 10:27:32 +0300

    Update profil.php

--- frontend/profil.php
+++ frontend/profil.php
@@ -19,7 +19,7 @@
       </div> 
         <div class="text-data">
             <!-- Aici vine php cu echo si script pentru email -->
-            <span class="name"><div id="name"></div></span>
+            <span class="name"><div id="name">1</div></span>
             <span class="jobs"><div id="skills">1</div></span>
         </div>
     </div>
@@ -128,4 +128,31 @@
 </html>
 <?php
 
-include "../backend/profilbk.php";
+session_start(); // Start the session
+include "conn.php"; echo"lol";
+if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
+    $email = $_SESSION['email'];
+    $password = $_SESSION['password'];
+
+    $query="SELECT * FROM users WHERE email = '{$email}'";
+    $select_user_query = mysqli_query($conn, $query);
+    
+    if(!$select_user_query)
+    {
+      die('Query Failed'. mysqli_error());
+    }
+    if(mysqli_num_rows($select_user_query) > 0) {
+        while($row = mysqli_fetch_assoc($select_user_query)) {
+            $db_email = $row['email'];
+            $db_password = $row['password'];
+            $db_name = $row['name'];
+            $db_skills = $row['skills'];
+            $db_image = $row['image'];
+        }
+    }
+    echo $db_name;
+    echo $db_skills;
+    echo $db_image;
+    echo $db_email;
+    echo $db_password;
+}
