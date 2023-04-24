6a726f20d1cb7be411b6c5c66fe52cf4ff6f2a5a
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-24 16:50:55 +0300

    Update profil.php

--- frontend/profil.php
+++ frontend/profil.php
@@ -21,7 +21,7 @@ include "../backend/profilbk.php";?>
                     <?php if($image==NULL)
                         echo "<img src='/img/user.png' class='profile-img'>"?>
                     <img src="data:image/jpeg;base64,<?=base64_encode($image)?>" alt="<?=$name?>" class="profile-img">
-
+                    </img>
             </div>
             <div class="text-data">
             <!-- Aici vine php cu echo si script pentru email -->
