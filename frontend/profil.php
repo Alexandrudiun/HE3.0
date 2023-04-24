4fc995ba0487d4ab0bb70cc3aeb6e38ef4a44a12
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-04-24 14:32:26 +0300

    Update profil.php

--- frontend/profil.php
+++ frontend/profil.php
@@ -19,9 +19,9 @@ include "../backend/profilbk.php";?>
             <div class="profile-card">
                 <div class="image">
                     <?php if($image==NULL)
-                        echo "<img src='/img/user.png' class='profile-img'>";
-                        else echo "<img src='data:image/jpeg;base64,<?=base64_encode($image)?>' alt='<?=$name?>' class='profile-img'>";
-                    ?>
+                        echo "<img src='/img/user.png' class='profile-img'>"?>
+                    <img src="data:image/jpeg;base64,<?=base64_encode($image)?>" alt="<?=$name?>" class="profile-img">
+
             </div>
             <div class="text-data">
             <!-- Aici vine php cu echo si script pentru email -->
