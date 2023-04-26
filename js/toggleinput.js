db11bdc32ce33ae6919f93edca85426a613f6eb2
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-26 23:31:37 +0300

    js bugs

--- js/toggleinput.js
+++ js/toggleinput.js
@@ -1,20 +1,63 @@
-function replaceColors() {
-  const elements = document.getElementsByTagName('*');
+const toggleButton = document.querySelector('#toggle-button');
+const originalStyles = {};
+
+function invertColor(hex) {
+  return '#' + hex.slice(1).split('').map(c => (15 - parseInt(c, 16)).toString(16)).join('');
+}
+
+function invertStyles() {
+  const elements = document.querySelectorAll('*');
   for (let i = 0; i < elements.length; i++) {
-    const style = window.getComputedStyle(elements[i]);
-    const background = style.getPropertyValue('background');
-    const boxShadow = style.getPropertyValue('box-shadow');
-
-    if (background === 'rgb(255, 152, 0)') {
-      elements[i].style.backgroundColor = '#e91e63';
+    const styles = getComputedStyle(elements[i]);
+    if (styles.backgroundColor && styles.backgroundColor !== 'rgba(0, 0, 0, 0)') {
+      originalStyles[elements[i]] = originalStyles[elements[i]] || {};
+      originalStyles[elements[i]].backgroundColor = styles.backgroundColor;
+      elements[i].style.backgroundColor = invertColor(styles.backgroundColor);
     }
-
-    if (boxShadow.includes('rgb(255, 152, 0)')) {
-      const modifiedBoxShadow = boxShadow.replace(/rgb\(255,\s*152,\s*0\)/g, '#e91e63');
-      elements[i].style.boxShadow = modifiedBoxShadow;
+    if (styles.boxShadow) {
+      originalStyles[elements[i]] = originalStyles[elements[i]] || {};
+      originalStyles[elements[i]].boxShadow = styles.boxShadow;
+      const boxShadowParts = styles.boxShadow.split(' ');
+      const invertedColor = invertColor(boxShadowParts[boxShadowParts.length - 1]);
+      boxShadowParts[boxShadowParts.length - 1] = invertedColor;
+      elements[i].style.boxShadow = boxShadowParts.join(' ');
+    }
+    if (styles.border && styles.border !== 'none') {
+      originalStyles[elements[i]] = originalStyles[elements[i]] || {};
+      originalStyles[elements[i]].border = styles.border;
+      const borderParts = styles.border.split(' ');
+      const invertedColor = invertColor(borderParts[borderParts.length - 1]);
+      borderParts[borderParts.length - 1] = invertedColor;
+      elements[i].style.border = borderParts.join(' ');
     }
   }
 }
 
-const toggleBtn = document.getElementById('toggle-button');
-toggleBtn.addEventListener('click', replaceColors);
+function revertStyles() {
+  const elements = document.querySelectorAll('*');
+  for (let i = 0; i < elements.length; i++) {
+    const styles = originalStyles[elements[i]];
+    if (styles) {
+      if (styles.backgroundColor) {
+        elements[i].style.backgroundColor = styles.backgroundColor;
+      }
+      if (styles.boxShadow) {
+        elements[i].style.boxShadow = styles.boxShadow;
+      }
+      if (styles.border) {
+        elements[i].style.border = styles.border;
+      }
+    }
+  }
+  originalStyles = {};
+}
+
+toggleButton.addEventListener('click', () => {
+  if (toggleButton.classList.contains('active')) {
+    toggleButton.classList.remove('active');
+    revertStyles();
+  } else {
+    toggleButton.classList.add('active');
+    invertStyles();
+  }
+});
