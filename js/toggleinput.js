e30123539ebbd3fc6cff67bbdc890935d728a416
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-26 23:27:14 +0300

    bugs js

--- js/toggleinput.js
+++ js/toggleinput.js
@@ -1,15 +1,20 @@
-const toggleButton = document.getElementById('toggle-button');
+function replaceColors() {
+  const elements = document.getElementsByTagName('*');
+  for (let i = 0; i < elements.length; i++) {
+    const style = window.getComputedStyle(elements[i]);
+    const background = style.getPropertyValue('background');
+    const boxShadow = style.getPropertyValue('box-shadow');
 
-toggleButton.addEventListener('click', function() {
-  const elements = document.querySelectorAll('*');
-  
-  for(let i = 0; i < elements.length; i++) {
-    const element = elements[i];
-    const styles = getComputedStyle(element);
-    const color = styles.getPropertyValue('color');
+    if (background === 'rgb(255, 152, 0)') {
+      elements[i].style.backgroundColor = '#e91e63';
+    }
 
-    if(color === 'rgb(255, 152, 0)') {
-      element.style.color = '#e91e63';
+    if (boxShadow.includes('rgb(255, 152, 0)')) {
+      const modifiedBoxShadow = boxShadow.replace(/rgb\(255,\s*152,\s*0\)/g, '#e91e63');
+      elements[i].style.boxShadow = modifiedBoxShadow;
     }
   }
-});
+}
+
+const toggleBtn = document.getElementById('toggle-button');
+toggleBtn.addEventListener('click', replaceColors);
