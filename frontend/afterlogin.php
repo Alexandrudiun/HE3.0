587804db963872de64e87f315a376658e5ec27fd
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-26 16:21:16 +0300

    afterlogin form select profile front-end

---
+++ frontend/afterlogin.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login | I&D CREW</title>
+  <link rel="icon" href="/logos/logo192.jpg">
+  <!-- Bootstrap -->
+  <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+  <link rel="stylesheet" href="/css/afterlogin.css">
+  <link rel="manifest" href="/manifest.json">
+</head>
+
+<body>
+<section>
+    <div class="form-box">
+      <div class="form-value">
+        <form action="#" method="Post">
+          <h1>Selectează tipul de profil</h1>
+          <div class="input-box">
+            <div>
+            <label>
+              <input type="checkbox" id="worker" name="worker">
+              <span>Lucrator</span>
+            </label>
+            </div>
+            <div>
+              <label>
+                <input type="checkbox" id="worker" name="worker">
+                <span>Cumpărator</span>
+              </label>
+            </div>
+          </div>
+          <div class="button">
+            <input type="submit" name="submit" value="Salvează" class="btn-login">
+        </form>
+        <div class="note">
+          <p>*pot fi selectate ambele opţiuni*</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -44,6 +44,6 @@
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
-</html>
+</html> 
 <?php
 include "backend/loginbk.php";
