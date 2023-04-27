056551a014075068a7fe6e89106704fcee3e6d02
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-27 22:36:07 +0300

    Update toggleinput.js

--- js/toggleinput.js
+++ js/toggleinput.js
@@ -1,9 +1,16 @@
 const toggleButton = document.querySelector('#toggle-button');
 
-
-toggleButton.addEventListener('click', () =>{
-  document.documentElement.style.setProperty('--worker-color' , '#e91e63');
+toggleButton.addEventListener('click', () => {
+  const workerColor = getComputedStyle(document.documentElement).getPropertyValue('--worker-color');
+  const prestatorColor = getComputedStyle(document.documentElement).getPropertyValue('--prestator-color');
+  
+  if (workerColor === prestatorColor) {
+    document.documentElement.style.setProperty('--worker-color', '');
+  } else {
+    document.documentElement.style.setProperty('--worker-color', prestatorColor);
+  }
 });
+
 
 
 
