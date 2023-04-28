12fa532385ebc5eacdea058d6cab9d73d43a8823
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-04-28 21:54:06 +0300

    Update loginbk.php

--- backend/loginbk.php
+++ backend/loginbk.php
@@ -17,8 +17,6 @@ include "conn.php";
       die('Query Failed'. mysqli_error());
     }
     $email_found = false; // Flag variable to check if email was found
-    $profile_type_set = false; // Flag variable to check if profile type is set
-    
     while($row = mysqli_fetch_row($select_user_query)){
         if($row[1] !== $email) {
             continue; // Skip to next row if email doesn't match
@@ -29,27 +27,16 @@ include "conn.php";
             $_SESSION['password'] = $password;
             if(!($row[8] == 2 && $row[9] == 2))
              { 
-               $profile_type_set = true;
+               header("Location: ../frontend/profiletype.php");
              }
-             else{
             echo "<script>document.getElementById('message').innerHTML = 'Te-ai logat cu succes';</script>";
             header("Location: /frontend/profil.php");
             exit;//  Exit script after successful login
-             }
         }
         else {
             echo "<script>document.getElementById('message').innerHTML = 'Parola incorecta';</script>";
             exit; // Exit script after unsuccessful login
         }
-    }
-    
-    if(!$email_found) {
-        echo "<script>document.getElementById('message').innerHTML = 'Email neînregistrat';</script>";
-    }
-    
-    if(!$profile_type_set) {
-        header("Location: ../frontend/profiletype.php");
-        exit;
     }
 
     if(!$email_found) {
