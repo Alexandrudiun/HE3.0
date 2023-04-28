d0b7bd75e1ab63960043fe5d745e7d10a6eaf70f
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-04-28 22:08:07 +0300

    Update profilbk.php

--- backend/profilbk.php
+++ backend/profilbk.php
@@ -20,10 +20,7 @@ if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
     $email_found = false; // Flag variable to check if email was found
     while($row = mysqli_fetch_row($select_user_query)) {
     if($row[1] == $email && $row[2] == $password) {
-      if(($row[8] == 2 && $row[9] == 2))
-      { 
-        header("Location: /frontend/profiletype.php");
-      }
+      
       $email_found = true;
       $image = $row[3]; //image is stored in blob format
       $name = $row[4];
