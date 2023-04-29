660ae89ad2090bff417ec80291f6784a559dca29
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-04-29 12:37:49 -0700

    Update profiletypebk.php

--- backend/profiletypebk.php
+++ backend/profiletypebk.php
@@ -6,8 +6,13 @@ if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
   if(isset($_POST['submit'])){
     $email = $_SESSION['email'];
     $password = $_SESSION['password'];
+
+
     $worker = isset($_POST['worker']) ? 1 : 0;
     $buyer = isset($_POST['buyer']) ? 1 : 0;
+
+
+
     $query = "UPDATE users SET worker = '$worker', buyer = '$buyer' WHERE email = '$email'";
     $update_user_query = mysqli_query($conn, $query);
     
