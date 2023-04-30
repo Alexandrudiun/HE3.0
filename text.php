d2f942db75e0690fc01604dac32f3f874841665e
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-04-30 19:23:20 +0300

    Update text.php

--- text.php
+++ text.php
@@ -1,21 +1,17 @@
 <?php
-$worker =1;
-$buyer =1;
-$profiletype_temp = 0;
-if ($worker == 1 && $buyer == 1) {
-    // Check if the button was clicked
-    if (isset($_POST['toggle_profile_type'])) {
-        // Toggle the profile type value
-        if($profiletype_temp == 0)$profiletype_temp = 1;
-        else $profiletype_temp = 0;
-    }
-    if($profiletype_temp) echo "worker";
-    else echo "buyer";
-    // Display the toggle button with the current profile type value
-    echo "<form method='post'><button type='submit' name='toggle_profile_type'>Switch profile type to $profiletype_temp</button></form>";
-} else if ($worker == 1 && $buyer == 0) {
+session_start();
+$profiletype_temp = isset($_SESSION['profiletype_temp']) ? $_SESSION['profiletype_temp'] : 0;
+
+if ($profiletype_temp) {
     echo "worker";
 } else {
     echo "buyer";
+}
+
+echo "<form method='post'><button type='submit' name='toggle_profile_type'>Switch profile type</button></form>";
+
+if (isset($_POST['toggle_profile_type'])) {
+    $profiletype_temp = $profiletype_temp ? 0 : 1;
+    $_SESSION['profiletype_temp'] = $profiletype_temp;
 }
 ?>
