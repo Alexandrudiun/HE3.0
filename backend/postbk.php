7b16b70b069dbe36dec04d64b0e1718f425e6a9c
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-04-30 23:14:05 +0300

    1

--- backend/postbk.php
+++ backend/postbk.php
@@ -16,14 +16,6 @@ if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
     $skills = $_POST['skills'];
 
     // Upload images to server
-    $target_dir = "uploads/";
-    $target_file1 = $target_dir . basename($_FILES["imagine1"]["name"]);
-    $target_file2 = $target_dir . basename($_FILES["imagine2"]["name"]);
-    $target_file3 = $target_dir . basename($_FILES["imagine3"]["name"]);
-    move_uploaded_file($_FILES["imagine1"]["tmp_name"], $target_file1);
-    move_uploaded_file($_FILES["imagine2"]["tmp_name"], $target_file2);
-    move_uploaded_file($_FILES["imagine3"]["tmp_name"], $target_file3);
-
     // Insert data into database
     $query = "INSERT INTO posts (email, name, title, price, description, imagine1, imagine2, imagine3, skills) VALUES ('{$email}', '{$name}', '{$title}', '{$price}', '{$description}', '{$imagine1}', '{$imagine2}', '{$imagine3}', '{$skills}')";
     $add_user_query = mysqli_query($conn, $query);
--- text.php
+++ text.php
@@ -1,17 +1,56 @@
 <?php
-session_start();
-$profiletype_temp = isset($_SESSION['profiletype_temp']) ? $_SESSION['profiletype_temp'] : 0;
+//session_start();
+//$profiletype_temp = isset($_SESSION['profiletype_temp']) ? $_SESSION['profiletype_temp'] : 0;
 
-if ($profiletype_temp) {
-    echo "worker";
-} else {
-    echo "buyer";
-}
+//if ($profiletype_temp) {
+//    echo "worker";
+//} else {
+//    echo "buyer";
+//}
 
-echo "<form method='post'><button type='submit' name='toggle_profile_type'>Switch profile type</button></form>";
+//echo "<form method='post'><button type='submit' name='toggle_profile_type'>Switch profile type</button></form>";
 
-if (isset($_POST['toggle_profile_type'])) {
-    $profiletype_temp = $profiletype_temp ? 0 : 1;
-    $_SESSION['profiletype_temp'] = $profiletype_temp;
-}
+//if (isset($_POST['toggle_profile_type'])) {
+//    $profiletype_temp = $profiletype_temp ? 0 : 1;
+//    $_SESSION['profiletype_temp'] = $profiletype_temp;
+//}
+
+
 ?>
+<form action="text.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <input type="submit" name="submit" value="Upload">
+</form>
+<?php
+if(isset($_POST['submit'])){
+    $file = $_FILES['file'];
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name']; 
+    $fileSize = $_FILES['file']['size']; 
+    $fileError = $_FILES['file']['error']; 
+    $fileType = $_FILES['file']['type']; 
+    
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+    
+    $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+    
+    if(in_array($fileActualExt, $allowed)){
+        if($fileError === 0){
+            if($fileSize < 1000000){
+                $fileNameNew = uniqid('', true).".".$fileActualExt;
+                $fileDestination = 'img/upload/'.$fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                header("Location: text.php?uploadsuccess");
+            } else {
+                echo "Your file is too big!";
+            }
+        } else {
+            echo "There was an error uploading your file!";
+        }
+    } else {
+        echo "You cannot upload files of this type!";
+    }
+}
+
+
