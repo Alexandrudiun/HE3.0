ef51a5ec948e754e0db217989f8e6dcb2ddcdf0c
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-04-30 19:25:40 +0300

    Update profil.php

--- frontend/profil.php
+++ frontend/profil.php
@@ -63,6 +63,24 @@ include "../backend/profilbk.php";?>
                     }
                          
                     ?>
+                    <?php
+session_start();
+$profiletype_temp = isset($_SESSION['profiletype_temp']) ? $_SESSION['profiletype_temp'] : 0;
+
+if ($profiletype_temp) {
+    echo "worker";
+} else {
+    echo "buyer";
+}
+
+echo "<form method='post'><button type='submit' name='toggle_profile_type'>Switch profile type</button></form>";
+
+if (isset($_POST['toggle_profile_type'])) {
+    $profiletype_temp = $profiletype_temp ? 0 : 1;
+    $_SESSION['profiletype_temp'] = $profiletype_temp;
+}
+?>
+
                 
                 <div class="credits credits-box">
                     <a href="#" class="credits">Credits: <?php echo $credit; ?> RON</a>
