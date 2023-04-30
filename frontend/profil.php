60c328b0b9ebdf458a8cc0b4e00eb58701003a28
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-04-30 19:51:16 +0300

    Update profil.php

--- frontend/profil.php
+++ frontend/profil.php
@@ -65,7 +65,7 @@ if (isset($_POST['toggle_profile_type'])) {
                 {
                                       
 ?> <p>Switch profile type to</p> <?php 
-                    if($profiletype_temp)   echo "worker";
+                    if(!$profiletype_temp)   echo "worker";
                     else echo "buyer";
                   }
                 else
