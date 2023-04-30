b7ffff82257d55e14f3382a434bf8d1672a13054
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-04-30 12:56:53 +0300

    form

---
+++ js/sliderimg.js
@@ -0,0 +1,24 @@
+let fileInput = document.getElementById("fileInput");
+let imageContainer = document.getElementById("images");
+let numOfFiles = document.getElementById("num-of-files");
+
+function preview (){
+  imageContainer.innerHTML = "";
+  numOfFiles.textContent = `${fileInput.files.length}
+  Imagini Selectate`;
+
+  for ( i of fileInput.files){
+    let reader = new FileReader();
+    let figure = document.createElement("figure");
+    let figCap = document.createElement("figCaption");
+    figCap.innerText = i.name;
+    figure.appendChild(figCap);
+    reader.onload=()=>{
+      let img = document.createElement("img");
+      img.setAttribute("src", reader.result);
+      figure.insertBefore(img,figCap);
+    }
+    imageContainer.appendChild(figure);
+    reader.readAsDataURL(i);
+  }
+}
