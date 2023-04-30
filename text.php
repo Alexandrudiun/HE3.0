0f77a0d731731dc234e7a9a9f13ff96fb3a78b87
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-04-30 12:22:43 +0300

    Update text.php

--- text.php
+++ text.php
@@ -6,7 +6,8 @@ if ($worker == 1 && $buyer == 1) {
     // Check if the button was clicked
     if (isset($_POST['toggle_profile_type'])) {
         // Toggle the profile type value
-        $profiletype_temp = ($profiletype_temp == 1) ? 0 : 1;
+        if($profiletype_temp == 0)$profiletype_temp = 1;
+        else $profiletype_temp = 0;
     }
     if($profiletype_temp) echo "worker";
     else echo "buyer";
