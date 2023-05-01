8ea52e74abc3c8d2d6a178d392bb99093dd68223
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-01 20:29:07 +0300

    Update postbk.php

--- backend/postbk.php
+++ backend/postbk.php
@@ -8,7 +8,8 @@ session_start();
 
 if(isset($_SESSION['email']) && isset ($_SESSION['password'])) 
  echo "You are logged in!";
-
+else
+ echo "You are not logged in!";
 if(isset($_POST['submit'])){
     $email = $_SESSION['email'];
     echo $email;
