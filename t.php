715112ca24df9079c0815c2b7c6356e2b9be22fc
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-01 20:17:44 +0300

    Update t.php

--- t.php
+++ t.php
@@ -3,10 +3,3 @@
     session_start(); // Start the session
 $email = $_SESSION['email'];
 $password = $_SESSION['password'];    
-if(isset($password) && isset($email))
-{echo "ok";}
-else {
- echo "not ok"
- echo $email;
-    echo $password;
-}
