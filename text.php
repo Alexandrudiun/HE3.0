5bba129e41af34aa02789d5ad54dbb638f5d1f78
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-01 16:44:45 +0300

    Update text.php

--- text.php
+++ text.php
@@ -1,16 +1,41 @@
 
+<form method="post" enctype="multipart/form-data">
+    <input type="file" name="file[]" multiple>
+    <input type="submit" name="submit" value="Upload">
+</form>
+
 <?php
 if(isset($_POST['submit'])){
-    //move_uploaded_file();
-    foreach($FILES['doc']['name'] as $key=>$val){
-        $rand=rand('11111111','99999999');
-        $file=$rand.''.$val;
-        move_uploaded_file($_FILES['doc']['tmp_name'][$key],'img/upload/'.$file);
-        //insert into table(image) values('$file');
+    for ($i = 0; $i < count($_FILES['file']['name']); $i++) {
+    $file = $_FILES['file'][$i];
+    $fileName = $_FILES['file']['name'][$i];
+    $fileTmpName = $_FILES['file']['tmp_name'][$i];
+    $fileSize = $_FILES['file']['size'][$i]; 
+    $fileError = $_FILES['file']['error'][$i]; 
+    $fileType = $_FILES['file']['type'][$i]; 
+    
+
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+    
+    $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+    
+    if(in_array($fileActualExt, $allowed)){
+        if($fileError === 0){
+            if($fileSize < 1000000){
+                $fileNameNew = uniqid('', true).".".$fileActualExt;
+                $fileDestination = 'img/upload/'.$fileNameNew;
+                move_uploaded_file($_FILES['file']['tmp_name'][$i]; , $fileDestination);
+                header("Location: text.php?uploadsuccess");
+            } else {
+                echo "Your file is too big!";
+            }
+        } else {
+            echo "There was an error uploading your file!";
+        }
+    } else {
+        echo "You cannot upload files of this type!";
     }
 }
-?>
-<form method="post" enctype="multipart/form-data">
-    <input type="file" name="doc[]" multiple/>
-    <input type="submit" name="submit"/>
-</form>
+
+}
