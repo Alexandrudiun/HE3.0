fc7ac54c57a62f0671b0a7eeaa1136d0d3d20459
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-01 13:05:02 +0300

    Update text.php

--- text.php
+++ text.php
@@ -39,7 +39,7 @@ if(isset($_POST['submit'])){
         if($fileError === 0){
             if($fileSize < 1000000){
                 $fileNameNew = uniqid('', true).".".$fileActualExt;
-                $fileDestination = 'img/'.$fileNameNew;
+                $fileDestination = '/'.$fileNameNew;
                 move_uploaded_file($fileTmpName, $fileDestination);
                 header("Location: text.php?uploadsuccess");
             } else {
