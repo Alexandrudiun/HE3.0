914ce5c0d547fcb0d041884afc89d72e2eb0a8f0
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-01 17:21:11 +0300

    Update post.php

--- frontend/post.php
+++ frontend/post.php
@@ -1,7 +1,44 @@
-
+<form action="text.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file[]" multiple>
+    <input type="submit" name="submit" value="Upload">
+</form>
 <?php 
+if(isset($_POST['submit'])){
+    $files = $_FILES['file'];
+    $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+    $uploadCount = 0;
 
-include "../backend/postbk.php";
+    foreach($files['name'] as $key => $fileName){
+        $fileTmpName = $files['tmp_name'][$key]; 
+        $fileSize = $files['size'][$key]; 
+        $fileError = $files['error'][$key]; 
+        $fileType = $files['type'][$key]; 
+
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+
+        if(in_array($fileActualExt, $allowed)){
+            if($fileError === 0){
+                if($fileSize < 1000000){
+                    $fileNameNew = uniqid('', true).".".$fileActualExt;
+                    $fileDestination = 'img/upload/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                    $uploadCount++;
+                } else {
+                    echo "File $fileName is too big!<br>";
+                }
+            } else {
+                echo "There was an error uploading file $fileName!<br>";
+            }
+        } else {
+            echo "You cannot upload files of type $fileActualExt!<br>";
+        }
+    }
+
+    if($uploadCount > 0){
+        header("Location: text.php?uploadsuccess");
+    }
+}
 
 
 ?><!DOCTYPE html>
