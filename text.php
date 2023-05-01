963590784adb577ff74fb5276331b4b780f27044
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-01 16:17:47 +0300

    Update text.php

--- text.php
+++ text.php
@@ -24,13 +24,14 @@
 
 <?php
 if(isset($_POST['submit'])){
-    for ($i = 0; $i < count($_FILES['file']['name']); $i++){
     $file = $_FILES['file'];
-    $fileName = $_FILES['file']['name'];
-    $fileTmpName = $_FILES['file']['tmp_name']; 
-    $fileSize = $_FILES['file']['size']; 
-    $fileError = $_FILES['file']['error']; 
-    $fileType = $_FILES['file']['type']; 
+    for ($i = 0; $i < count($_FILES['file']['name']); $i++){
+   
+        $fileName = $_FILES['files']['name'][$i];
+        $fileTmpName = $_FILES['files']['tmp_name'][$i]; 
+        $fileSize = $_FILES['files']['size'][$i]; 
+        $fileError = $_FILES['files']['error'][$i]; 
+        $fileType = $_FILES['files']['type'][$i]; 
     
     $fileExt = explode('.', $fileName);
     $fileActualExt = strtolower(end($fileExt));
