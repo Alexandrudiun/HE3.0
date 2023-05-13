14b44e692e6112288305e1baee3cdc52963e5d91
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-13 15:51:53 +0300

    Update conn.php

--- backend/conn.php
+++ backend/conn.php
@@ -8,10 +8,3 @@ $dbName="u312656249_idcrew";
 
 $conn= mysqli_connect($dbServername, $dbUsername, $dbPassoword, $dbName);
 
-if($conn){
-    echo "Connection succesful";   
-    }
-    else{
-        echo "Connection failed";
-    }
-}
