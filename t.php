2c89d577e73fa567664054ea9855bef5fdccb3e9
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-13 15:30:15 +0300

    Update t.php

--- t.php
+++ t.php
@@ -1,7 +1,47 @@
-<?php
 
-    session_start(); // Start the session
-$email = $_SESSION['email'];
-$password = $_SESSION['password'];    
- echo $email;
- echo $password;
+<div id="product-container"><?php
+
+include "conn.php";
+
+
+$sql = "SELECT * FROM posts";
+$result = mysqli_query($conn, $sql);
+
+// Store the products in an array
+$post = array();
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $post[] = $row;
+    }
+}
+$location="img/upload/" . $post['images'];
+foreach ($post as $post) {
+    echo '<div class="post">';
+    echo '<h2>' . $post['name'] . '</h2>';
+    echo '<p>' . $post['description'] . '</p>';
+    echo '<img src="' . $location . '" alt="' . $post['name'] . '">';
+    echo '<p>Price: ' . $post['price'] . '</p>';
+    echo '<p>Skills required: ' . $post['skills'] . '</p>';
+    echo '</div>';
+}
+?>
+</div>
+<style>
+    #product-container {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.product {
+    width: 300px;
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+}
+
+.product img {
+    width: 100%;
+    height: auto;
+}
+
+</style>
