afcececa25763e5f61b91dd3d0e74a8f86ad5f46
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-13 16:00:51 +0300

    Update t.php

--- t.php
+++ t.php
@@ -12,12 +12,13 @@ $post = array();
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         $post[] = $row;
-        $location="img/upload/" . $row['images']; // Moved inside the while loop
+        $photo_names = explode(',', $row['images']);
+        $location="img/upload/" . $photo_names[1]; // Moved inside the while loop
         echo '<div class="post">';
         echo '<h2>' . $row['name'] . '</h2>';
         echo '<p>' . $row['description'] . '</p>';
-        //echo '<img src="' . $location . '" alt="' . $row['name'] . '">';
-        echo $location;
+        echo '<img src="' . $location . '" alt="' . $row['name'] . '">';
+      
         echo '<p>Price: ' . $row['price'] . '</p>';
         echo '<p>Skills required: ' . $row['skills'] . '</p>';
         echo '</div>';
