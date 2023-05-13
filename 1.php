468c94a93de0bd883690039d072e149f52112d17
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-13 20:22:46 +0300

    12

--- 1.php
+++ 1.php
@@ -34,10 +34,7 @@ if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         $post[] = $row;
         $photo_names = explode(', ', $row['images']);
-        if($photo_names[0] == '')
-        {
-            $photo_names[0] = $row['images'];
-        }
+   
         $location="https://idcrew.shop/img/upload/" . $photo_names[0]; // Moved inside the while loop
         
       
