976d581852e0e0fe6b02d631cd501ad87229a245
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-13 16:10:31 +0300

    1

--- t.php
+++ t.php
@@ -1,35 +1,5 @@
 
-<div id="product-container"><?php
-
-include "backend/conn.php";
-
-
-$sql = "SELECT * FROM `posts` ORDER BY `id` ASC;";
-$result = mysqli_query($conn, $sql);
-
-// Store the products in an array
-$post = array();
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $post[] = $row;
-        $photo_names = explode(', ', $row['images']);
-        $location="https://idcrew.shop/img/upload/" . $photo_names[1]; // Moved inside the while loop
-        echo '<div class="post">';
-        echo '<h2>' . $row['name'] . '</h2>';
-        echo '<p>' . $row['description'] . '</p>';
-        echo '<img src="' . $location . '" alt="' . $row['name'] . '">';
-        //echo $location;
-        echo '<p>Price: ' . $row['price'] . '</p>';
-        echo '<p>Skills required: ' . $row['skills'] . '</p>';
-        echo '</div>';
-    }
-}
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-?>
+<div id="product-container">
 <h1>Products</h1>
 </div>
 <style>
