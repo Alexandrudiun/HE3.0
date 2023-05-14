48e137b7733b63bb6ea62807fb334cf3703d06e4
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-05-14 15:12:46 +0300

    js search

--- js/search.js
+++ js/search.js
@@ -1,15 +1,14 @@
 const search = () => {
   const searchbox = document.getElementById("search-item").value.toUpperCase();
-  const storeitems = document.getElementById("service-list");
   const product = document.querySelectorAll(".card");
 
-  for (var i=0; i < product.length; i++) {
-      let textvalue = product[i].textContent || product[i].innerText;
+  for (var i = 0; i < product.length; i++) {
+    let textvalue = product[i].textContent || product[i].innerText;
 
-      if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
-          product[i].style.display = "";
-      } else {
-          product[i].style.display = "";
-      }
+    if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+      product[i].classList.remove("hidden");
+    } else {
+      product[i].classList.add("hidden");
+    }
   }
-}
+};
