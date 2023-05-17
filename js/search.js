ddb7ebaba0cd58ee06ccd01c4491be2aed65cab8
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-05-17 20:14:35 +0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,47 +1,17 @@
-const binarySearch = (arr, searchValue) => {
-  let start = 0;
-  let end = arr.length - 1;
-
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
-    let textValue = arr[mid].textContent || arr[mid].innerText;
-
-    if (textValue.toUpperCase() === searchValue) {
-      return mid; // Element found at index mid
-    } else if (textValue.toUpperCase() < searchValue) {
-      start = mid + 1; // Search in the right half
-    } else {
-      end = mid - 1; // Search in the left half
-    }
-  }
-
-  return -1; // Element not found
-};
+// for better search performances use binary search O(logn);
 
 const search = () => {
-  const searchBox = document.getElementById("search-item").value.toUpperCase();
-  const product = Array.from(document.querySelectorAll(".card"));
+  const searchbox = document.getElementById("search-item").value.toUpperCase();
+  const product = document.querySelectorAll(".card");
 
-  product.sort((a, b) => {
-    const textA = a.textContent || a.innerText;
-    const textB = b.textContent || b.innerText;
-    return textA.localeCompare(textB);
-  });
+  for (var i = 0; i < product.length; i++) {
+    let textvalue = product[i].textContent || product[i].innerText;
 
-  const searchIndex = binarySearch(product, searchBox);
-
-  if (searchIndex !== -1) {
-    // Element found
-    product.forEach((item, index) => {
-      if (index === searchIndex) {
-        item.classList.remove("hidden");
-      } else {
-        item.classList.add("hidden");
-      }
-    });
-  } else {
-    // Element not found
-    product.forEach((item) => item.classList.add("hidden"));
+    if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+      product[i].classList.remove("hidden");
+    } else {
+      product[i].classList.add("hidden");
+    }
   }
 };
 
