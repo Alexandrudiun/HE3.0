9f15d8125d4bdf661a55559e7d58143c13b8617f
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-18 20:00:40 +0300

    1

--- frontend/history.php
+++ frontend/history.php
@@ -23,20 +23,62 @@
     <div class="flex-2-column">
       <div class="cards">
         <div class="flex-container" id="service-list">
-            <a href="/frontend/service.php?id=' . $row['id'] . ">
-              <div class="card">
-              <img src="/img/caine1.jpg">
-              <div class="info-area">
-                <div class="title-price">
-                <h3 class="service-title">Plimb caini zona tomis</h3>
-                <h2 class="service-price">2 500 lei</h2>
-                </div>
-                <div class="date">
-                <span>12:48</span>
-                <span>Constanta</span>
-                </div>
-              </div>
-            </div>
+
+
+          
+<?php
+
+
+include "conn.php";
+session_start();
+
+
+if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
+
+    $email=$_SESSION['email'];
+
+
+    $query="SELECT * FROM posts WHERE email = '{$email}'";
+    $result=mysqli_query($conn,$query);
+
+    $post = array();
+if (mysqli_num_rows($result) > 0) {
+echo '<div class="flex-container id="service-list">';
+while ($row = mysqli_fetch_assoc($result)) {
+$post[] = $row;
+$photo_names = explode(', ', $row['images']);
+$location="https://idcrew.shop/img/upload/" . $photo_names[0]; // Moved inside the while loop
+
+echo '<div class="card">';
+echo '<img src="' . $location . '" alt="' . $row['name'] . '" style="width: 100%;">';
+echo '<div class="info-area">';
+echo '<a href="/backend/historyedit.php"><button>edit</button></a>';
+echo '<h3 class="service-title">' . $row['title'] . '</h3>';
+echo '<h2 class="service-price">' . $row['price'] . '</h2>';
+echo '<span>' . $row['location'] . 'aici trb ceva (locatie era) </span>';
+echo '<span>' . $row['date'] . 'aici era data</span>';
+echo '</div>';
+echo '</div>';        
+
+}
+echo '</div>';
+}
+}
+          echo '<a href="/frontend/service.php?id="'. $row['id'] '>';
+          echo '<div class="card">';
+          echo '<img src="' . $location . '" alt="' . $row['name'] . '" style="width: 100%;">';
+          echo '<div class="info-area">';
+          echo '<div class="title-price">';
+          echo '<h3 class="service-title">Plimb caini zona tomis</h3>';
+          echo '<h2 class="service-price">2 500 lei</h2>';
+          echo '</div>';
+          echo '<div class="date">';
+          echo '<span>12:48</span>';
+          echo '<span>Constanta</span>';
+          echo '</div>';
+          echo '</div>';
+          echo '</div>';
+                ?>
           </a>
           <div class="delete-btn">
             <ion-icon name="close-outline"></ion-icon>
