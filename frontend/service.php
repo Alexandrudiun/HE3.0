1ef9a5a32a4acdeb9f9393571e10052f04b9466c
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-18 17:40:12 +0300

    Update service.php

--- frontend/service.php
+++ frontend/service.php
@@ -75,7 +75,27 @@ if (mysqli_num_rows($result) > 0) {
   echo '<p>'.$row['description'].'</p>';
   echo '</div>';
    }
- include "../backend/profilbk.php";
+ //
+ $query="SELECT * FROM users WHERE email = '{$row['email']}'";
+    $select_user_query = mysqli_query($conn, $query);
+    
+    if(!$select_user_query)
+    {
+      die('Query Failed');
+    }
+    $email_found = false; // Flag variable to check if email was found
+    while($row = mysqli_fetch_row($select_user_query)) {
+    if($row[1] == $email) {
+      
+      $email_found = true;
+      $image = $row[3]; //image is stored in blob format
+      $name = $row[4];
+      $skills = $row[5];
+      $credit = $row[6];
+      $worker = $row[7];
+      $buyer = $row[8];
+      
+    }
 }
 } else {
  
