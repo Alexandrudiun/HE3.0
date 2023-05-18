d91f1f618c6b8ddeabfc2dd503e0236030bb9127
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-18 20:26:02 +0300

    Update history.php

--- frontend/history.php
+++ frontend/history.php
@@ -36,14 +36,14 @@ session_start();
 if(isset($_SESSION['email']) && isset($_SESSION['password'])) {
 
     $email=$_SESSION['email'];
-echo $email;  
+
 
     $query="SELECT * FROM posts WHERE email = '{$email}'";
     $result=mysqli_query($conn,$query);
 
     $post = array();
 if (mysqli_num_rows($result) > 0) {
-echo '<div class="flex-container id="service-list">';
+
 while ($row = mysqli_fetch_assoc($result)) {
 $photo_names = explode(', ', $row['images']);
 $location="https://idcrew.shop/img/upload/" . $photo_names[0]; // Moved inside the while loop
@@ -51,7 +51,7 @@ $location="https://idcrew.shop/img/upload/" . $photo_names[0]; // Moved inside t
           
           echo '<div class="card">';
          // echo '<a href="/frontend/serviceedit.php?id="'. $row['id'] '>';
-         // echo '<img src="' . $location . '" alt="' . $row['name'] . '" style="width: 100%;">';
+          echo '<img src="' . $location . '" alt="' . $row['name'] . '" style="width: 100%;">';
         //   echo '<div class="info-area">';
         //   echo '<div class="title-price">';
         //   echo '<h3 class="service-title">'.$row['title'].'</h3>';
