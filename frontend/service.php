31107501f7c011cd84bc010d8e07371c40b0ff6a
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-05-18 02:04:52 -0700

    Update service.php

--- frontend/service.php
+++ frontend/service.php
@@ -30,8 +30,41 @@ if (isset($_GET['id'])) {
     <link rel="manifest" href="/manifest.json">
 </head>
 <body>
+<?php
+
+include "../backend/conn.php";
+
+
+$sql = "SELECT * FROM `posts` WHERE id = '{$id}';";
+$result = mysqli_query($conn, $sql);
+
+// Store the products in an array
+$post = array();
+if (mysqli_num_rows($result) > 0) {
+    echo '<div class="flex-container id="service-list">';
+    while ($row = mysqli_fetch_assoc($result)) {
+        $photo_names = explode(', ', $row['images']);
+        $location="https://idcrew.shop/img/upload/" . $photo_names[0]; 
+        echo '<div class="card">';
+        echo '<a style="text-decoration:none;" href="/frontend/service.php?id=' . $row['id'] . '">';
+        echo '<img src="' . $location . '" alt="' . $row['name'] . '" style="width: 100%;">';
+        echo '<div class="info-area">';
+        echo '<h3 class="service-title">' . $row['title'] . '</h3>';
+        echo '<h2 class="service-price">' . $row['price'] . '</h2>';
+        echo '<span>' . $row['location'] . 'aici trb ceva (locatie era) </span>';
+        echo '<span>' . $row['date'] . 'aici era data</span>';
+        echo '</div>';
+        echo '</a>';
+        echo '</div>';        
+        
+    }
+    echo '</div>';
+}
+
+?>
   <section class="container">
-    <div class="slide-wrapper">
+    <div class="
+    slide-wrapper">
       <div class="slider">
         <img id="slide-1" src="/img/caine1.jpg" >
         <img id="slide-2" src="/img/caine2.jpg" >
@@ -44,6 +77,10 @@ if (isset($_GET['id'])) {
       </div>
     </div>
   </section>
+
+
+
+  
   <main>
     <div class="top-details">
       <div class="date">
