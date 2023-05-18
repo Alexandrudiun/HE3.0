6342928451f6727ed9d1c8470eab5d08ca57bba8
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-18 17:48:38 +0300

    Update service.php

--- frontend/service.php
+++ frontend/service.php
@@ -79,25 +79,25 @@ if (mysqli_num_rows($result) > 0) {
    ?>
   <?php
   $email = $row['email'];
-  
-   $query="SELECT * FROM users WHERE email = '{$email}'";
-      $select_user_query = mysqli_query($conn, $query);
+   echo $email;
+  //  $query="SELECT * FROM users WHERE email = '{$email}'";
+  //     $select_user_query = mysqli_query($conn, $query);
       
-      if(!$select_user_query)
-      {
-        die('Query Failed');
-      }
-      $email_found = false; // Flag variable to check if email was found
-      while($row = mysqli_fetch_row($select_user_query)) {
-      if($row[1] == $email) {
+  //     if(!$select_user_query)
+  //     {
+  //       die('Query Failed');
+  //     }
+  //     $email_found = false; // Flag variable to check if email was found
+  //     while($row = mysqli_fetch_row($select_user_query)) {
+  //     if($row[1] == $email) {
         
-        $email_found = true;
-        $image = $row[3]; //image is stored in blob format
-        $name = $row[4];
-        $skills = $row[5];
+  //       $email_found = true;
+  //       $image = $row[3]; //image is stored in blob format
+  //       $name = $row[4];
+  //       $skills = $row[5];
        
         
-      }?>
+  //     }?>
 <?php
 }
 
