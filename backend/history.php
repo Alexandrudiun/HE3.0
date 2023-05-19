afe24ccd1a9a6f8194bbd39c9e7137b10978d681
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-05-19 00:49:08 -0700

    Update history.php

--- backend/history.php
+++ backend/history.php
@@ -47,7 +47,7 @@ $photo_names = explode(', ', $row['images']);
 $location="https://idcrew.shop/img/upload/" . $photo_names[0]; // Moved inside the while loop
 
 echo '<div class="card">';
-echo '<img src="' . $location . '" alt="' . $row['name'] . '" style="width: 100%;">';
+echo '<img src="' . $location . '" alt="' . $row['name'] . '" >';
 echo '<div class="info-area">';
 echo '<h3 class="service-title">' . $row['title'] . '</h3>';
 echo '<h2 class="service-price">' . $row['price'] . '</h2>';
