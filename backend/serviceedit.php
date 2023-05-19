cb3b00f8530d5295b960625b89f6316b08bca2e4
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-05-19 01:50:36 -0700

    Update serviceedit.php

--- backend/serviceedit.php
+++ backend/serviceedit.php
@@ -16,13 +16,154 @@ if(isset($_SESSION['email']) && isset($_SESSION['password']))
 
 
         if($row['email']==$_SESSION['email'])
-        {}}}?>
+        {
+
+       ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+    <link rel="icon" href="/img/logo192.jpg">
+    <link rel="stylesheet" href="/css/post.css">
+    <link rel="stylesheet" href="/css/navbar.css">
+    <link rel="manifest" href="/manifest.json">
+</head>
+<body>
+
+<main>
+        <div class="center">
+            <div class="profile-card">
+              <form action="serviceedit.php" method="post" enctype="multipart/form-data">
+                <h1>Publică un anunţ nou</h1>
+                <h3>Detalii anunţ</h3>
+                <section>
+                <div class="file-box file-input">
+                    <input type="file" id="fileInput" accept="image/png, image/png, image/jpeg" name="file[]" multiple="multiple" onchange="preview()" required>
+                    <label for="fileInput" class="inputFile">
+                        <ion-icon name="albums-outline"></ion-icon> Adaugă imagini
+                    </label>
+                    <p id="num-of-files">Nicio imagine adăugată</p>
+                    <div id="images"><?php
+
+
+                    $photo_names = explode(', ', $row['images']);
+        echo '<div class="slide-wrapper">';
+        echo '<div class="slider">';
+        for($i=0;$i<3;$i++){
+          $location="https://idcrew.shop/img/upload/" . $photo_names[$i]; 
+        echo '<img src="' . $location . '" alt="' . $row['name'] . '"  id="slide-'.$i.'">';
+        }?>
+                    </div>
+                </div>
+                <div id="mesaj" style="color:red; text-align:center;"></div>
+                </section>
+                <div class="input-box">
+                  <label for="">Titlul anunţului</label>
+                  <input type="text" id="titlu" name="titlu" value="<?php echo $row['title']; ?>" required>
+                </div>
+                <div class="input-box">
+                  <label for="">Descriere</label>
+                  <textarea type="text" id="descriere" name="descriere" rows="5" required></textarea>
+                </div>
+                <div class="input-box">
+                  <label for="">Preţ (RON)</label>
+                  <input type="number" id="pret" name="pret" value="<?php echo $row['price']; ?>" required>
+                </div>
+                    <h3>Date de contact</h3>
+                    <div class="input-box">
+                    <label for="">Telefon</label>
+                <input type="tel" id="phone" name="phone" pattern="^(\+40|0)[0-9]{9}$" required>
+                    </div>
+                <div class="input-box">
+                <label for="">Localitate</label>
+
+
+                <?php 
+                $parts = explode(', ', $city);
+
+                // Assign values to separate variables
+                $localitate = $parts[0];
+                $judet = $parts[1];
+                
+                ?>
+
+
+                <input type="text" id="localitate" name="localitate" value="<?php echo $localitate ?>" required>
+                </div>
+                <div class="input-box">
+                <label for="">Judeţ</label>
+                <input type="text" id="judet" name="judet" value="<?php echo $judet?>" required>
+                </div>
+                <div class="button">
+                  <input type="submit" name="submit" value="Post" class="btn-login">
+                  </div>
+                  
+              </form>
+            </div>
+          </div> 
+    </main>
+    <!-- Navbar Down --> 
+    <footer>  
+    <section class="nav-bar">
+        <div class="navigation">
+            <ul>
+                <li class="list">
+                    <a href="/frontend/home.php">
+                        <span class="icon">
+                        <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="text">Acasă</span>
+                    </a>
+                </li>
+                <li class="list">
+                    <a href="/frontend/profil.php">
+                        <span class="icon">
+                        <ion-icon name="person-circle-outline"></ion-icon>
+                        </span>
+                        <span class="text">Profil</span>
+                    </a>
+                </li>
+                <li class="list active">
+                    <a href="/backned/post.php">
+                        <span class="icon">
+                            <ion-icon name="add-circle-outline"></ion-icon>
+                        </span>
+                        <span class="text">Postează</span>
+                    </a>
+                </li>
+                <li class="list">
+                    <a href="/backend/history.php">
+                        <span class="icon">
+                            <ion-icon name="albums-outline"></ion-icon>
+                        </span>
+                        <span class="text">Istoric</span>
+                    </a>
+                </li>
+                <div class="indicator"></div>
+            </ul>
+        </div>
+    </section>
+    </footer>
+    <script src="/js/nav.js"></script>
+    <script src="/js/sliderimg.js"></script>
+  <!-- Ion icons -->
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
+
+
 
 
 
 
 <?php
-
+ }}}
 
 
 
