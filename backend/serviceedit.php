7b0eaf662747c2aa0cc0b39b7345fa81022c773b
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-05-19 01:35:41 -0700

    Update serviceedit.php

--- backend/serviceedit.php
+++ backend/serviceedit.php
@@ -1,19 +1,15 @@
-<?php 
+<?php
 session_start();
-if(isset($_SESSION['email']) && isset($_SESSION['password']))
-{
+if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
- echo $id;
         $sql = "SELECT * FROM `posts` WHERE id = '{$id}';";
         $result = mysqli_query($conn, $sql);
-        
-        
+
         while ($row = mysqli_fetch_assoc($result)) {
-        print_r($row);
-        echo "da";
+            print_r($row);
+            echo "da";
         }
-        print_r($row);
-        echo"nu";
     }
 }
+?>
