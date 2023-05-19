c8b8b13a803e28f475b9c78fd672e5daaf41ed47
Andrei Ionescu <andreiionescu750@gmail.com>
2023-05-19 17:13:02 +0300

    Update sliderimg.js

--- js/sliderimg.js
+++ js/sliderimg.js
@@ -2,30 +2,38 @@ let fileInput = document.getElementById("fileInput");
 let imageContainer = document.getElementById("images");
 let numOfFiles = document.getElementById("num-of-files");
 
-function preview (){
+function preview() {
   imageContainer.innerHTML = "";
-  numOfFiles.textContent = `${fileInput.files.length}
-  Imagini Selectate`;
+  numOfFiles.textContent = `${fileInput.files.length} Imagini Selectate`;
 
-  for ( i of fileInput.files){
+  for (let i of fileInput.files) {
     let reader = new FileReader();
     let figure = document.createElement("figure");
-    let figCap = document.createElement("figCaption");
+    let figCap = document.createElement("figcaption");
     figCap.innerText = i.name;
     figure.appendChild(figCap);
-    reader.onload=()=>{
+
+    reader.onload = () => {
       let img = document.createElement("img");
       img.setAttribute("src", reader.result);
-      figure.insertBefore(img,figCap);
-    }
+      figure.insertBefore(img, figCap);
+    };
+
+    let removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", () => {
+      removeImage(i);
+    });
+    figure.appendChild(removeButton);
+
     imageContainer.appendChild(figure);
     reader.readAsDataURL(i);
   }
 }
 
-function remove_img() {
+function removeImage(file) {
   let selectedFiles = Array.from(fileInput.files);
-  let removedIndex = selectedFiles.findIndex(file => file.name === fileInput.files[0].name);
+  let removedIndex = selectedFiles.findIndex((f) => f.name === file.name);
 
   if (removedIndex !== -1) {
     selectedFiles.splice(removedIndex, 1);
