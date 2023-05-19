0f656c7fb79518ce6ea39707fcf671be2b481ae6
Alexandru Dinu <alexandrudinu04@gmail.com>
2023-05-19 20:56:18 +0300

    Update serviceedit.php

--- backend/serviceedit.php
+++ backend/serviceedit.php
@@ -92,8 +92,8 @@ if(isset($_POST['submit'])){
                 echo "File $location cannot be deleted due to an error<br>";
               }
               }
-            $sql = "DELETE FROM `posts` WHERE `posts`.`id` = '{$id}';";
-            $select_user_query = mysqli_query($conn, $query);
+            //$sql = "DELETE FROM `posts` WHERE `posts`.`id` = '{$id}';";
+           // $select_user_query = mysqli_query($conn, $query);
             echo "<script>document.getElementById('mesaj').innerHTML = 'Anunțul a fost postat';</script>";
             header("Location: serviceedit.php?uploadsuccess");
         } else {
