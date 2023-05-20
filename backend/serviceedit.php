fdd0cdade4033ce21617996460f82cf163a93900
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-05-20 14:36:17 +0300

    Update serviceedit.php

--- backend/serviceedit.php
+++ backend/serviceedit.php
@@ -172,7 +172,7 @@ if(isset($_POST['submit'])){
                   </div>
                   
               </form>
-              <form action="del.php" method="post">
+              <form action="del?<?php echo $id;?>.php" method="post">
                 <div class="button">
                     <input type="submit" name="submit" value="Sterge Anuntul" class="btn-login">
                     </div>
