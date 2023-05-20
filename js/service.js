83c30b6cf0126ab273f3558b73a18f60e2670c03
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-05-20 15:02:13 +0300

    Update service.js

--- js/service.js
+++ js/service.js
@@ -2,7 +2,7 @@
     function showImage(index) {
     var popup = document.getElementById('popup-image');
     var popupSlide = document.getElementById('popup-slide-' + index);
-    popup.style.display = 'block';
+    popup.style.display = 'flex';
     popupSlide.style.display = 'block';
     }
 
