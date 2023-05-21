2cd3ca42aa180b9a5f40c8bc3c357af7a739c15c
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-05-21 12:42:49 +0300

    1

---
+++ frontend/serviceedit.php
@@ -0,0 +1,19 @@
+<?php if(isset($_SESSION['email']) && isset($_SESSION['password']))
+{ 
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        echo '<a style="text-decoration:none;" href="/backend/serviceedittext.php?id=' . $_SESSION['id'] . '">';
+        echo '<button></button>';
+        echo '</a>';
+        echo '<a style="text-decoration:none;" href="/backend/serviceeditServiceedittextAndPhotos.php?id=' . $_SESSION['id'] . '">';
+        echo '<button></button>';
+        echo '</a>';
+        
+    }
+else {
+    header("Location: ../backend/history.php");
+} 
+}
+    else {
+        header("Location: ../index.php");
+    } ?>
