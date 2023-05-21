81b2ab496f7ac5caac9848f173d63ce3bafab785
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2023-05-21 16:16:13 +0300

    Update profiletype.php

--- frontend/profiletype.php
+++ frontend/profiletype.php
@@ -25,13 +25,13 @@ include "../backend/profiletypebk.php";
             <div>
             <label>
               <input type="checkbox" id="worker" name="worker">
-              <span>Cumpărator (employer)</span>
+              <span>Vânzător (worker)</span>
             </label>
             </div>
             <div>
               <label>
                 <input type="checkbox" id="worker" name="buyer">
-                <span>Vânzător (worker)</span>
+                <span>Cumpărator (employer)</span>
               </label>
             </div>
           </div>
