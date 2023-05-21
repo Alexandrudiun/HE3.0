a3d38c7c4193e28aaa402bc6598de19d49528944
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-05-21 15:43:52 +0300

    Update profil.php

--- frontend/profil.php
+++ frontend/profil.php
@@ -76,7 +76,8 @@ if (isset($_POST['toggle_profile_type'])) {
     
         <div class="btn-container">
         <?php     
-
+$_SESSION['worker'] = $worker;
+$_SESSION['buyer'] = $buyer;
 if($worker == 1 && $buyer == 1) 
 {
     if(!$profiletype_temp)    echo '<a href="/backend/post.php" class="btn-post">Post a job</a>';
@@ -146,46 +147,93 @@ echo '<a href="/backend/post.php" class="btn-post">Post a job</a>';
                 </li>
                 
                 <?php
-                if(!$profiletype_temp){
-                    // for worker
-                echo '<li class="list">';
-                echo   '<a href="/backend/post.php">';
-                echo       '<span class="icon">';
-                echo            '<ion-icon name="add-circle-outline"></ion-icon>';
-                echo       '</span>';
-                echo        '<span class="text">Postează</span>';
-                echo   '</a>';
-                echo'</li>';
-
-                echo '<li class="list">';
-                echo   '<a href="/backend/history.php">';
-                echo       '<span class="icon">';
-                echo          '<ion-icon name="albums-outline"></ion-icon>';
-                echo'</span>';
-                echo    '<span class="text">Istoric</span>';
-                echo '</a>';
-                echo '</li>';
-            }
-                else{
-                    // for buyer
+                if($worker == 1 && $buyer == 1) 
+                {
+                    if(!$profiletype_temp){
+                        // for worker
                     echo '<li class="list">';
-                    echo   '<a href="/frontend/join.php">';
+                    echo   '<a href="/backend/post.php">';
                     echo       '<span class="icon">';
                     echo            '<ion-icon name="add-circle-outline"></ion-icon>';
                     echo       '</span>';
-                    echo        '<span class="text">Alătură-te echipei</span>';
+                    echo        '<span class="text">Postează</span>';
                     echo   '</a>';
                     echo'</li>';
-
-                echo '<li class="list">';
-                echo   '<a href="/frontend/contact.php">';
-                echo       '<span class="icon">';
-                echo          '<ion-icon name="albums-outline"></ion-icon>';
-                echo'</span>';
-                echo    '<span class="text">Contact</span>';
-                echo '</a>';
-                echo '</li>';
+    
+                    echo '<li class="list">';
+                    echo   '<a href="/backend/history.php">';
+                    echo       '<span class="icon">';
+                    echo          '<ion-icon name="albums-outline"></ion-icon>';
+                    echo'</span>';
+                    echo    '<span class="text">Istoric</span>';
+                    echo '</a>';
+                    echo '</li>';
                 }
+                    else{
+                        // for buyer
+                        echo '<li class="list">';
+                        echo   '<a href="/frontend/join.php">';
+                        echo       '<span class="icon">';
+                        echo            '<ion-icon name="add-circle-outline"></ion-icon>';
+                        echo       '</span>';
+                        echo        '<span class="text">Alătură-te echipei</span>';
+                        echo   '</a>';
+                        echo'</li>';
+    
+                    echo '<li class="list">';
+                    echo   '<a href="/frontend/contact.php">';
+                    echo       '<span class="icon">';
+                    echo          '<ion-icon name="albums-outline"></ion-icon>';
+                    echo'</span>';
+                    echo    '<span class="text">Contact</span>';
+                    echo '</a>';
+                    echo '</li>';
+                    }
+                
+                }
+                
+                else{
+                    if($worker == 1 && $buyer == 0) {
+                         // for worker
+                        echo '<li class="list">';
+                        echo   '<a href="/backend/post.php">';
+                        echo       '<span class="icon">';
+                        echo            '<ion-icon name="add-circle-outline"></ion-icon>';
+                        echo       '</span>';
+                        echo        '<span class="text">Postează</span>';
+                        echo   '</a>';
+                        echo'</li>';
+        
+                        echo '<li class="list">';
+                        echo   '<a href="/backend/history.php">';
+                        echo       '<span class="icon">';
+                        echo          '<ion-icon name="albums-outline"></ion-icon>';
+                        echo'</span>';
+                        echo    '<span class="text">Istoric</span>';
+                        echo '</a>';
+                        echo '</li>';
+                    }
+                    else{
+                        // for buyer
+                        echo '<li class="list">';
+                        echo   '<a href="/frontend/join.php">';
+                        echo       '<span class="icon">';
+                        echo            '<ion-icon name="add-circle-outline"></ion-icon>';
+                        echo       '</span>';
+                        echo        '<span class="text">Alătură-te echipei</span>';
+                        echo   '</a>';
+                        echo'</li>';
+    
+                    echo '<li class="list">';
+                    echo   '<a href="/frontend/contact.php">';
+                    echo       '<span class="icon">';
+                    echo          '<ion-icon name="albums-outline"></ion-icon>';
+                    echo'</span>';
+                    echo    '<span class="text">Contact</span>';
+                    echo '</a>';
+                    echo '</li>';
+                    }
+                    }
                 ?>
                 <div class="indicator"></div>
             </ul>
