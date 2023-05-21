17503b2609303b8157f4047c9578a66d4efd3d05
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-05-21 12:50:46 +0300

    Update serviceedit.php

--- backend/serviceedit.php
+++ backend/serviceedit.php
@@ -8,7 +8,7 @@ if(isset($_SESSION['email']) && isset($_SESSION['password']))
         echo '<button>Editeaza doar contintul text al Anuntului</button>';
         echo '</a>';
         echo '<a style="text-decoration:none;" href="/backend/ServiceedittextAndPhotos.php?id=' . $id. '">';
-        echo '<button>Editeaza doar tot Anuntulu</button>';
+        echo '<button>Editeaza tot Anuntulu</button>';
         echo '</a>';
         
     }
