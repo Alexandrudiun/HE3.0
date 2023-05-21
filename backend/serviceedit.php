23e34073a4b9e486cff98d79ec95d69245911ccd
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2023-05-21 12:47:47 +0300

    Update serviceedit.php

--- backend/serviceedit.php
+++ backend/serviceedit.php
@@ -4,10 +4,10 @@ if(isset($_SESSION['email']) && isset($_SESSION['password']))
 { 
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
-        echo '<a style="text-decoration:none;" href="/backend/serviceeditext.php?id=' . $_SESSION['id'] . '">';
+        echo '<a style="text-decoration:none;" href="/backend/serviceeditext.php?id=' . $id . '">';
         echo '<button></button>';
         echo '</a>';
-        echo '<a style="text-decoration:none;" href="/backend/ServiceedittextAndPhotos.php?id=' . $_SESSION['id'] . '">';
+        echo '<a style="text-decoration:none;" href="/backend/ServiceedittextAndPhotos.php?id=' . $id. '">';
         echo '<button></button>';
         echo '</a>';
         
