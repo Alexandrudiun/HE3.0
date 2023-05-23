cfdd720b8e294d86dbec4308ae890761368c6440
Andrei Ionescu <andreiionescu750@gmail.com>
2023-05-23 11:05:26 +0300

    Update searchhistory.js

--- js/searchhistory.js
+++ js/searchhistory.js
@@ -1,35 +1,16 @@
 const search = () => {
   const searchbox = document.getElementById("search-item").value.toUpperCase();
-  const cards = document.querySelectorAll(".card");
+  const products = document.querySelectorAll(".card");
   let visibleCount = 0;
 
-  for (let i = 0; i < cards.length; i++) {
-    let textValue = cards[i].textContent || cards[i].innerText;
+  for (let i = 0; i < products.length; i++) {
+    let textValue = products[i].textContent || products[i].innerText;
 
     if (textValue.toUpperCase().indexOf(searchbox) > -1) {
-      cards[i].classList.remove("hidden");
+      products[i].classList.remove("hidden");
       visibleCount++;
     } else {
-      cards[i].classList.add("hidden");
+      products[i].classList.add("hidden");
     }
-  }
-
-  const marginContent = document.querySelectorAll(".card .margin-bottom");
-  marginContent.forEach(content => {
-    content.style.display = "none";
-  });
-
-  const deleteButtons = document.querySelectorAll(".card .delete-btn");
-  deleteButtons.forEach(button => {
-    button.style.display = "none";
-  });
-
-  if (visibleCount === 1) {
-    const visibleCard = document.querySelector(".card:not(.hidden)");
-    visibleCard.style.width = "100%";
-  } else {
-    cards.forEach(card => {
-      card.style.width = "calc(50% - 20px)";
-    });
   }
 };
