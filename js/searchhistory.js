f6bda65800dac58d79dba4c5967dd629ea60054c
Andrei Ionescu <andreiionescu750@gmail.com>
2023-05-23 10:47:15 +0300

    Update searchhistory.js

---
+++ js/searchhistory.js
@@ -0,0 +1,16 @@
+const search = () => {
+  const searchbox = document.getElementById("search-item").value.toUpperCase();
+  const products = document.querySelectorAll(".card");
+  let visibleCount = 0;
+
+  for (let i = 0; i < products.length; i++) {
+    let textValue = products[i].textContent || products[i].innerText;
+
+    if (textValue.toUpperCase().indexOf(searchbox) > -1) {
+      products[i].classList.remove("hidden");
+      visibleCount++;
+    } else {
+      products[i].classList.add("hidden");
+    }
+  }
+};
