f57a9903ddd13fabf3894d4ae6e2444157185e96
Andrei Ionescu <andreiionescu750@gmail.com>
2023-05-23 11:21:21 +0300

    .

--- js/searchhistory.js
+++ js/searchhistory.js
@@ -1,16 +1,21 @@
 const search = () => {
   const searchbox = document.getElementById("search-item").value.toUpperCase();
-  const products = document.querySelectorAll(".card");
+  const cards = document.querySelectorAll(".card");
+  const deleteButtons = document.querySelectorAll(".delete-btn");
   let visibleCount = 0;
 
-  for (let i = 0; i < products.length; i++) {
-    let textValue = products[i].textContent || products[i].innerText;
+  for (let i = 0; i < cards.length; i++) {
+    let textValue = cards[i].textContent || cards[i].innerText;
+    let deleteButton = deleteButtons[i];
 
     if (textValue.toUpperCase().indexOf(searchbox) > -1) {
-      products[i].classList.remove("hidden");
+      cards[i].classList.remove("hidden");
+      deleteButton.classList.remove("hidden");
       visibleCount++;
     } else {
-      products[i].classList.add("hidden");
+      cards[i].classList.add("hidden");
+      deleteButton.classList.add("hidden");
     }
   }
 };
+
